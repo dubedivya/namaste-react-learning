e50206294b65c569bd59c0030e2ff3c6b2bfb82d
dubedivya <dubedivya1992@gmail.com>
2023-06-04 18:08:12 +0530

    Competing Episode 05 with useState Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
-const Body = () => (
-  <div className="body">
-    <div className="Search">
-      <input type="text" name="Search" value="" placeholder="" />
-      <input type="submit" name="Search" value="Search" />
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  return (
+    <div className="body">
+      <div className="tools">
+        <div className="Search">
+          <input type="text" name="Search" value="" placeholder="" />
+          <input type="submit" name="Search" value="Search" />
+        </div>
+        <div className="filter">
+          <button
+            className="filter-btn"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter(
+                (res) => res.data.avgRating > 4
+              );
+              console.log(filteredList);
+              setListOfRestaurants(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+      </div>
+
+      <div className="res-card-container">
+        {listOfRestaurants.map((resData) => (
+          <RestaurantCard key={resData.data.id} resData={resData} />
+        ))}
+      </div>
     </div>
-    <div className="res-card-container">
-      {resList.map((resData) => (
-        <RestaurantCard key={resData.data.id} resData={resData} />
-      ))}
-    </div>
-  </div>
-);
+  );
+};
 
 export default Body;
